fec135f881ce217952a02712b0e12ce4cbbff896
Sekwon Lee <sekwon.lee@hpe.com>
2026-02-09 13:23:35 -0600

    Restore example command in add_publication.py for clarity

--- scripts/README.md
+++
@@ -1,3 +0,0 @@
-```
-$ python3 scripts/add_publication.py --input 'type: conference; filename: "my_paper_2026"; title: "Title"; authors: "A, B, <b>Name</b>"; venue: "Proceedings of ..."; date: "2026-01-15"; urls: {paper: "http://sekwonlee.github.io/paper.pdf", code: "http://sekwonlee.github.io/code.pdf", slides: "http://sekwonlee.github.io/slides.pdf"}'
-```
--- scripts/add_publication.py
+++ scripts/add_publication.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# $ python3 scripts/add_publication.py --input 'type: conference; filename: "my_paper_2026"; title: "Title"; authors: "A, B, <b>Name</b>"; venue: "Proceedings of ..."; date: "2026-01-15"; urls: {paper: "http://sekwonlee.github.io/paper.pdf", code: "http://sekwonlee.github.io/code.pdf", slides: "http://sekwonlee.github.io/slides.pdf"}'
 import argparse
 import re
 from urllib.parse import urlparse
